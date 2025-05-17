0ee69606660ab3f7e0a01e5f4bbb8cd68367d436
IsaacCartuche <isaac.cartuche@unl.edu.ec>
2025-05-17 12:10:07 -0500

    Add Dao Models
    agregación de los Dao para cada modelo

---
+++ src/main/java/com/mistletoe/estaciona/base/controller/dao/dao_models/DaoCliente.java
@@ -0,0 +1,44 @@
+package com.mistletoe.estaciona.base.controller.dao.dao_models;
+
+import com.mistletoe.estaciona.base.controller.dao.AdapterDao;
+import com.mistletoe.estaciona.base.models.Cliente;
+
+public class DaoCliente extends AdapterDao<Cliente>{
+    private Cliente obj;
+
+    public DaoCliente() {
+        super(Cliente.class);
+        //TODO Auto-generated constructor stub
+    }
+
+    public Cliente getObj() {
+        return this.obj;
+    }
+
+    public void setObj(Cliente obj) {
+        this.obj = obj;
+    }
+
+    public Boolean save() {
+        try {
+            obj.setId(listAll().getLength()+1);
+            this.persist(obj);
+            return true;
+        } catch (Exception e) {
+            //TODO
+            return false;
+            // TODO: handle exception
+        }
+    }
+
+    public Boolean update(Integer pos) {
+        try {
+            this.update(obj, pos);
+            return true;
+        } catch (Exception e) {
+            //TODO
+            return false;
+            // TODO: handle exception
+        }
+    }
+}
