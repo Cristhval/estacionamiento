e49d7f449f7beac7ba1975a52ea318b2c133cf8c
IsaacCartuche <isaac.cartuche@unl.edu.ec>
2025-05-17 13:00:58 -0500

    Add Atributes
    se agrego atributo cedula a la clase cliente, y el atributo ruc a la clase administrador

--- src/main/java/com/mistletoe/estaciona/base/models/Administrador.java
+++ src/main/java/com/mistletoe/estaciona/base/models/Administrador.java
@@ -2,6 +2,15 @@ package com.mistletoe.estaciona.base.models;
 
 public class Administrador extends Usuario{
     private Integer idParqueadero;
+    private String ruc;
+
+    public String getRuc() {
+        return this.ruc;
+    }
+
+    public void setRuc(String ruc) {
+        this.ruc = ruc;
+    }
 
     public Administrador(Integer id, String nombre, String apellido, String correoElectronico, Integer idParqueadero) {
         super(id, nombre, apellido, correoElectronico);
--- src/main/java/com/mistletoe/estaciona/base/models/Cliente.java
+++ src/main/java/com/mistletoe/estaciona/base/models/Cliente.java
@@ -1,6 +1,16 @@
 package com.mistletoe.estaciona.base.models;
 
 public class Cliente extends Usuario{
+    private String cedula;
+
+    public String getCedula() {
+        return this.cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
     public Cliente(Integer id, String nombre, String apellido, String correoElectronico) {
         super(id, nombre, apellido, correoElectronico);
     }
